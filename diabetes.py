dab881ef4dad4e47b55e02931bab3f07ff77a670
Ant2Mor <AntonMorken@gmail.com>
2025-03-26 13:02:09 +0100

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv(r"C:\Users\Anton\Downloads\diabetes.csv")
+    df = pd.read_csv("diabetes.csv")
     df = df.rename(columns={"Diabetes_012": "Diabetes"})
     return df
 
